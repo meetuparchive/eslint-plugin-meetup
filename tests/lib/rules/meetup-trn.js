7ea1d53bbe58a656984cea9d68ad3857db9c0c02
Michael McGahan <mike.mcgahan@gmail.com>
2016-12-31 13:07:36 +1300

    initial commit

---
+++ tests/lib/rules/meetup-trn.js
@@ -0,0 +1,36 @@
+/**
+ * @fileoverview An ESLint plugin for validating trn message definitions in web platform apps
+ * @author Michael McGahan
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/meetup-trn"),
+
+    RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+ruleTester.run("meetup-trn", rule, {
+
+    valid: [
+        "defineMessages({ fooMessage: { id: 'foo', defaultMessage: 'bar', description: { text: 'foo bar', jira: '1234' } } });",
+    ],
+
+    invalid: [
+        {
+          code: "defineMessages({ fooMessage: { id: 'foo', defaultMessage: 'bar', description: { text: 'foo bar' } } });",
+            errors: [{
+                message: "each message description must have a 'jira' property",
+                type: "Property"
+            }]
+        }
+    ]
+});
